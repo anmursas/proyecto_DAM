83e8ab1ba3fb21437f5d33439519ae0c51c8879c
andres <a.murcia@cps.es>
2022-06-01 14:14:59 +0200

    Not Found exception

--- backend/src/main/java/procesos/springboot/controller/ProcesoController.java
+++ backend/src/main/java/procesos/springboot/controller/ProcesoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import procesos.springboot.exception.ResourceNotFoundException;
 import procesos.springboot.model.Proceso;
 import procesos.springboot.model.ProcesoCandidatos;
 import procesos.springboot.model.Reclutador;
